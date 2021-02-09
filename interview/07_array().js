d2b979112cc6af714e3b0357325bf0a57aac3dd4
antim-stream <antim@stream.space>
2021-02-09 08:26:32 +0530

    array methods

--- interview/07_array().js
+++ interview/07_array().js
@@ -18,6 +18,137 @@ const registeredUsers = data.filter(user => user.isRegistered);
 
 
 
+// Q. Explain array methods [ join(), pop(), push(), shift(), unshift(), concat(), map(), filter(), reduce(), reduceRight(), every(), some(), indexOf(), lastIndexOf(), find(), findIndex(), includes() ]
+// a.) array.join(): The join() method creates and returns a new string by concatenating all of the elements in an array (or an array-like object), separated by commas or a specified separator string. If the array has only one item, then that item will be returned without using the separator.
+
+var elements = ['Fire', 'Air', 'Water'];
+
+console.log(elements.join()); // Output: "Fire,Air,Water"
+console.log(elements.join('')); // Output: "FireAirWater"
+console.log(elements.join('-')); // Output: "Fire-Air-Water"
+
+
+// b.) array.pop(): The pop() method removes the last element from an array and returns that element. This method changes the length of the array.
+
+var plants = ['broccoli', 'cauliflower', 'kale'];
+
+console.log(plants.pop()); // Output: "kale"
+console.log(plants); // Output: Array ["broccoli", "cauliflower"]
+console.log(plants.pop()); // Output: "cauliflower"
+console.log(plants.pop()); // Output: "broccoli"
+console.log(plants.pop()); // Output: "undefined"
+// c.) array.push(): The push() method adds one or more elements to the end of an array and returns the new length of the array.
+
+const animals = ['pigs', 'goats', 'sheep'];
+
+const count = animals.push('cows');
+console.log(count); // Output: 4
+console.log(animals); // Output: Array ["pigs", "goats", "sheep", "cows"]
+// d.) array.shift(): The shift() method removes the first element from an array and returns that removed element. This method changes the length of the array.
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.shift();
+console.log(fruits) // Output: Array ["Orange", "Apple", "Mango"]
+// e.) array.unshift(): The unshift() method adds one or more elements to the beginning of an array and returns the new length of the array.
+
+var fruits = ["Banana", "Orange", "Apple"];
+fruits.unshift("Mango","Pineapple");
+console.log(fruits); // Output: Array ["Mango", "Pineapple", "Banana", "Orange", "Apple"]
+// f.) array.concat(): The concat() method is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
+
+const array1 = ['a', 'b', 'c'];
+const array2 = ['d', 'e', 'f'];
+
+console.log(array1.concat(array2)); // Output: Array ["a", "b", "c", "d", "e", "f"]
+// g.) array.map(): The map() method creates a new array with the results of calling a provided function on every element in the calling array.
+
+var array1 = [1, 4, 9, 16];
+
+// pass a function to map
+const map1 = array1.map(x => x * 2); 
+
+console.log(map1); // Output: Array [2, 8, 18, 32]
+// h.) array.filter(): The filter() method creates a new array with all elements that pass the test implemented by the provided function.
+
+var words = ['spray', 'limit', 'elite', 'exuberant', 'destruction'];
+
+const result = words.filter(word => word.length > 6);
+
+console.log(result); // Output: Array ["exuberant", "destruction"]
+// i.) array.reduce(): The reduce() method executes a reducer function (that you provide) on each element of the array, resulting in a single output value.
+
+const array1 = [1, 2, 3, 4];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+console.log(array1.reduce(reducer)); // Output: 10
+console.log(array1.reduce(reducer, 5)); // Output: 15
+// j.) array.reduceRight(): The reduceRight() method applies a function against an accumulator and each value of the array (from right-to-left) to reduce it to a single value.
+
+const array1 = [[0, 1], [2, 3], [4, 5]].reduceRight(
+  (accumulator, currentValue) => accumulator.concat(currentValue)
+);
+
+console.log(array1); // Output: Array [4, 5, 2, 3, 0, 1]
+// k.) array.every(): The every() method tests whether all elements in the array pass the test implemented by the provided function. It returns a Boolean value.
+
+function isBelowThreshold(currentValue) {
+  return currentValue < 40;
+}
+
+var array1 = [1, 30, 39, 29, 10, 13];
+console.log(array1.every(isBelowThreshold)); // Output: true
+// l.) array.some(): The some() method tests whether at least one element in the array passes the test implemented by the provided function. It returns a Boolean value.
+
+var array = [1, 2, 3, 4, 5];
+
+var even = function(element) {
+  // checks whether an element is even
+  return element % 2 === 0;
+};
+
+console.log(array.some(even)); // Output: true
+// m.) array.indexOf(): The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.
+
+var beasts = ['ant', 'bison', 'camel'];
+
+console.log(beasts.indexOf('camel')); // Output: 2
+console.log(beasts.indexOf('giraffe')); // Output: -1
+// n.) array.lastIndexOf(): The lastIndexOf() method returns the index within the calling String object of the last occurrence of the specified value, searching backwards from fromIndex. Returns -1 if the value is not found.
+
+var paragraph = 'The quick brown fox jumps over the lazy dog. If the dog barked, was it really lazy?';
+
+var searchTerm = 'dog';
+
+console.log('The index of the first "' + searchTerm + '" from the end is ' + paragraph.lastIndexOf(searchTerm));
+// Output: "The index of the first "dog" from the end is 52"
+// o.) array.find(): The find() method returns the value of the first element in the provided array that satisfies the provided testing function.
+
+var array1 = [5, 12, 8, 130, 44];
+
+var found = array1.find(function(element) {
+  return element > 100;
+});
+
+console.log(found); // Output: 130
+// p.) array.findIndex(): The findIndex() method returns the index of the first element in the array that satisfies the provided testing function. Otherwise, it returns -1, indicating that no element passed the test.
+
+var array1 = [5, 12, 8, 130, 44];
+
+function isLargeNumber(element) {
+  return element > 20;
+}
+
+console.log(array1.findIndex(isLargeNumber)); // Output: 3
+// q.) array.includes(): The includes() method determines whether an array includes a certain value among its entries, returning true or false as appropriate.
+
+var array1 = [1, 2, 3];
+console.log(array1.includes(2)); // Output: true
+
+var pets = ['cat', 'dog', 'bat'];
+console.log(pets.includes('at')); // Output: false
+
+
+
 
 const strs = ["I", " ", "am", " ", "Iron", " ", "Man"];
 const result = strs.reduce((acc, currentStr) => acc + currentStr, "");
